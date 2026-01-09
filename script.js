8e37bbbe79a460457a1b53ccb24d1dd142b7165e
shresile <46599124+shresile@users.noreply.github.com>
2026-01-09 14:21:31 +0800

    Complete the final logic to call 5 times
    .The function playRound() and two variables of choices of human and computer
    are moved in new function playGame() ;
    .The two variables' declaration method was const, now it is changed to let,
    because these variables will get updates.
    .The process repeats 5 times, there is a score announcement each round.

--- script.js
+++ script.js
@@ -32,44 +32,79 @@ function getHumanChoice(){
 
 
 
-let humanScore = 0;
-let computerScore = 0;
+playGame();
 
-function playRound(humanChoice,ComputerChoice){
-    if (humanChoice ==ComputerChoice){
-        const newHumanSelection = getHumanChoice();
-        console.log(`new!user: ${newHumanSelection}`);
-        const newComputerSelection = getComputerChoice();
-        console.log(`new!computer: ${newComputerSelection}`);
-        playRound(newHumanSelection, newComputerSelection);
+function playGame(){
 
-    } else if (humanChoice == "rock" && ComputerChoice == "paper"){
-        computerScore++;
-        console.log("You lose! Paper beats Rock.");
-    } else if (humanChoice == "rock" && ComputerChoice == "scissors"){
-        humanScore++;
-        console.log("You win! Rock beats Scissors.");
-    } else if (humanChoice == "paper" && ComputerChoice == "scissors"){
-        computerScore++;
-        console.log("You lose! Scissors beats Paper.");
-    } else if (humanChoice == "paper" && ComputerChoice == "rock"){
-        humanScore++;
-        console.log("You win! Paper beats Rock.");
-    } else if (humanChoice == "scissors" && ComputerChoice == "rock"){
-        computerScore++;
-        console.log("You lose! Rock beats Scissors.");
-    } else {
-        humanScore++;
-        console.log("You win! Scissors beats Paper.");
-    } 
+    let humanScore = 0;
+    let computerScore = 0;
+
+    let humanSelection = getHumanChoice();
+    console.log(`user: ${humanSelection}`);
+    let computerSelection = getComputerChoice();
+    console.log(`computer: ${computerSelection}`);
+    playRound(humanSelection, computerSelection);
+    console.log(`Round 1, your score is ${humanScore}, computer's score is ${computerScore}!`);
+
+    humanSelection = getHumanChoice();
+    console.log(`user: ${humanSelection}`);
+    computerSelection = getComputerChoice();
+    console.log(`computer: ${computerSelection}`);
+    playRound(humanSelection, computerSelection);
+    console.log(`Round 2, your score is ${humanScore}, computer's score is ${computerScore}!`);
+
+    humanSelection = getHumanChoice();
+    console.log(`user: ${humanSelection}`);
+    computerSelection = getComputerChoice();
+    console.log(`computer: ${computerSelection}`);
+    playRound(humanSelection, computerSelection);
+    console.log(`Round 3, your score is ${humanScore}, computer's score is ${computerScore}!`);
+
+    humanSelection = getHumanChoice();
+    console.log(`user: ${humanSelection}`);
+    computerSelection = getComputerChoice();
+    console.log(`computer: ${computerSelection}`);
+    playRound(humanSelection, computerSelection);
+    console.log(`Round 4, your score is ${humanScore}, computer's score is ${computerScore}!`);
+
+    humanSelection = getHumanChoice();
+    console.log(`user: ${humanSelection}`);
+    computerSelection = getComputerChoice();
+    console.log(`computer: ${computerSelection}`);
+    playRound(humanSelection, computerSelection);
+    console.log(`Round 5, your score is ${humanScore}, computer's score is ${computerScore}!`);
+
+
+
+    function playRound(humanChoice,ComputerChoice){
+        if (humanChoice ==ComputerChoice){
+            const newHumanSelection = getHumanChoice();
+            console.log(`new!user: ${newHumanSelection}`);
+            const newComputerSelection = getComputerChoice();
+            console.log(`new!computer: ${newComputerSelection}`);
+            playRound(newHumanSelection, newComputerSelection);
+
+        } else if (humanChoice == "rock" && ComputerChoice == "paper"){
+            computerScore++;
+            console.log("You lose! Paper beats Rock.");
+        } else if (humanChoice == "rock" && ComputerChoice == "scissors"){
+            humanScore++;
+            console.log("You win! Rock beats Scissors.");
+        } else if (humanChoice == "paper" && ComputerChoice == "scissors"){
+            computerScore++;
+            console.log("You lose! Scissors beats Paper.");
+        } else if (humanChoice == "paper" && ComputerChoice == "rock"){
+            humanScore++;
+            console.log("You win! Paper beats Rock.");
+        } else if (humanChoice == "scissors" && ComputerChoice == "rock"){
+            computerScore++;
+            console.log("You lose! Rock beats Scissors.");
+        } else {
+            humanScore++;
+            console.log("You win! Scissors beats Paper.");
+        } 
+    }
 }
 
 
 
-const humanSelection = getHumanChoice();
-console.log(`user: ${humanSelection}`);
-const computerSelection = getComputerChoice();
-console.log(`computer: ${computerSelection}`);
-
-playRound(humanSelection, computerSelection);
-console.log(`Now, your score is ${humanScore}, computer's score is ${computerScore}!`);

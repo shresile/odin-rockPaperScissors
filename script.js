bb62e8a5f7b6086d870752f644cc64f3234e3e6d
shresile <46599124+shresile@users.noreply.github.com>
2026-01-09 12:29:45 +0800

    Add functions getHumanChoice(),playRound()
    Now this project supports one single round, and the logic is right.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello!");
+
 function getComputerChoice(){
     let num = Math.floor(Math.random()*3);
     switch (num){
@@ -14,4 +14,55 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice ());
+
+function getHumanChoice(){
+    let num = Number(prompt("enter a num, 1 for \"rock\", 2 for \"paper\" or 3 for \"scissors\""));
+    switch (num){
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    }
+}
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,ComputerChoice){
+    if (humanChoice ==ComputerChoice){
+        return;
+    } else if (humanChoice == "rock" && ComputerChoice == "paper"){
+        computerScore++;
+        console.log("You lose! Paper beats Rock.");
+    } else if (humanChoice == "rock" && ComputerChoice == "scissors"){
+        humanScore++;
+        console.log("You win! Rock beats Scissors.");
+    } else if (humanChoice == "paper" && ComputerChoice == "scissors"){
+        computerScore++;
+        console.log("You lose! Scissors beats Paper.");
+    } else if (humanChoice == "paper" && ComputerChoice == "rock"){
+        humanScore++;
+        console.log("You win! Paper beats Rock.");
+    } else if (humanChoice == "scissors" && ComputerChoice == "rock"){
+        computerScore++;
+        console.log("You lose! Rock beats Scissors.");
+    } else {
+        humanScore++;
+        console.log("You win! Scissors beats Paper.");
+    } 
+}
+
+const humanSelection = getHumanChoice();
+console.log(`user: ${humanSelection}`);
+const computerSelection = getComputerChoice();
+console.log(`computer: ${computerSelection}`);
+
+playRound(humanSelection, computerSelection);
+console.log(`Now, your score is ${humanScore}, computer's score is ${computerScore}!`);
